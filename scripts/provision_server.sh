bf89c9cfa537a2f5be49cf3122385d7b0dcb3741
Tenagrim <tenagrim@gmail.com>
2022-10-01 15:34:01 +0500

    install net-tools for ifconfig

--- scripts/provision_server.sh
+++ scripts/provision_server.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $1 --node-ip $1  --bind-address=$1 --advertise-address=$1"
-#export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
-
+[ -f '/vagrant/node-token' ] && rm '/vagrant/node-token'
+sudo yum install net-tools -y
 curl -sfL https://get.k3s.io | sh -
 sudo mkdir -p /etc/rancher/k3s
 sudo cp /vagrant/config/registries.yaml /etc/rancher/k3s/registries.yaml
-#sudo chmod +r /etc/rancher/k3s/k3s.yaml
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant
