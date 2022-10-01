f7d54354fdb67d811abef0fd7d36a8f650916929
Tenagrim <tenagrim@gmail.com>
2022-10-01 19:08:32 +0500

    task 3 initial

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,8 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-MASTER_NAME = 'gshonaS'
+MASTER_NAME = 'p3-gshonaS'
 MASTER_IP = '192.168.42.110'
-
-WORKER_NAME = 'gshonaSW'
-WORKER_IP = '192.168.42.111'
-
 VM_MEMORY =  "1024"
 VM_CPUS = 1
 
@@ -18,29 +14,13 @@ Vagrant.configure("2") do |config|
     control.vm.hostname = MASTER_NAME
     control.vm.network :private_network, ip: MASTER_IP
     control.vm.synced_folder ".", "/vagrant", type: "virtualbox"
-    #control.vm.synced_folder ".", "/vagrant", disabled: false
     control.vm.provision :shell, privileged: true, path: "scripts/provision_server.sh", args: [MASTER_IP]
     control.vm.provider "virtualbox" do |v|
-      #v.customize ["modifyvm", :id, "--name", "wilS"]
       v.name = MASTER_NAME
       v.gui = false
       v.memory = VM_MEMORY
       v.cpus = VM_CPUS
     end
   end
-
-
-    config.vm.define WORKER_NAME do |control|
-      control.vm.hostname = WORKER_NAME
-      control.vm.network :private_network, ip: WORKER_IP
-      control.vm.synced_folder ".", "/vagrant", type: "virtualbox"
-      control.vm.provision :shell, privileged: true, path: "scripts/provision_worker.sh", args: [MASTER_IP, WORKER_IP]
-      control.vm.provider "virtualbox" do |v|
-        v.name = WORKER_NAME
-        v.gui = false
-        v.memory = VM_MEMORY
-        v.cpus = VM_CPUS
-      end
-    end
 
 end
--- scripts/provision_server.sh
+++ scripts/provision_server.sh
@@ -1,9 +1,56 @@
 #!/bin/bash
 
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $1 --node-ip $1  --bind-address=$1 --advertise-address=$1"
-[ -f '/vagrant/node-token' ] && rm '/vagrant/node-token'
+export PATH=$PATH:/usr/local/bin
 sudo yum install net-tools -y
-curl -sfL https://get.k3s.io | sh -
-sudo mkdir -p /etc/rancher/k3s
-sudo cp /vagrant/config/registries.yaml /etc/rancher/k3s/registries.yaml
-sudo cp /var/lib/rancher/k3s/server/node-token /vagrant
+
+# ###############################################################
+# Docker installtion:
+# Ref:  https://docs.docker.com/engine/install/centos/
+
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+#sudo groupadd docker
+sudo usermod -aG docker vagrant
+sudo systemctl start docker
+
+# ###############################################################
+# Kubectl install
+# Ref: https://kubernetes.io/ru/docs/tasks/tools/install-kubectl/
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
+chmod +x ./kubectl && \
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# ###############################################################
+# K3D install
+# Ref: https://k3d.io/v5.4.6/
+
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# ###############################################################
+# K3D cluster
+
+k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 8888:8888@loadbalancer --port 8443:443@loadbalancer
+
+
+# ###############################################################
+# Argo CD install:
+# Ref: https://argo-cd.readthedocs.io/en/stable/getting_started/
+
+kubectl create namespace argocd
+kubectl create namespace dev
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
+
+#kubectl wait --for=condition=Ready --timeout=-1s  pods --all -n argocd
+
+# ###########
+# Ingress:
+# Ref: https://argo-cd.readthedocs.io/en/stable/operator-manual/ingress/
+
+kubectl apply -n argocd -f /vagrant/config/argocd_ingress.yaml
+kubectl apply -n argocd -f /vagrant/config/argocd_application_deploy.yaml
+
+# latest at 01.10.2022: https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.12/manifests/install.yaml
