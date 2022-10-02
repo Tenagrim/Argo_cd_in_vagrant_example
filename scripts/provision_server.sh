2ecad526cf50de3296348051d5f18a72aa384e0c
Tenagrim <tenagrim@gmail.com>
2022-10-02 13:15:00 +0500

    task 3 done

--- scripts/provision_server.sh
+++ scripts/provision_server.sh
@@ -32,8 +32,20 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 # ###############################################################
 # K3D cluster
+# ########################
+# Calico Instead of Flanel
+# Ref: https://k3d.io/v5.3.0/usage/advanced/calico/
 
-k3d cluster create dev-cluster   --port 8080:80@loadbalancer --port 8888:8888@loadbalancer --port 8443:443@loadbalancer
+sudo mkdir -p /config
+curl -O https://k3d.io/v5.3.0/usage/advanced/calico.yaml
+sudo mv calico.yaml /config
+
+clustername=dev-cluster
+k3d cluster create "${clustername}" \
+  --k3s-arg '--flannel-backend=none@server:*' \
+  --volume "/config/calico.yaml:/var/lib/rancher/k3s/server/manifests/calico.yaml"
+
+#k3d cluster create dev-cluster   --port 8080:80@loadbalancer --port 8888:8888@loadbalancer --port 8443:443@loadbalancer
 
 sudo cp -r /root/.kube /home/vagrant
 sudo chown 1000:1000 /home/vagrant/.kube
@@ -46,10 +58,8 @@ sudo chmod 666 /home/vagrant/.kube/config
 
 kubectl create namespace argocd
 kubectl create namespace dev
-#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.12/manifests/install.yaml
 # latest at 01.10.2022: https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.12/manifests/install.yaml
-#kubectl wait --for=condition=Ready --timeout=-1s  pods --all -n argocd
 
 kubectl wait --for=condition=Ready --timeout=-1s  pods --all -n argocd
 # ###########
@@ -59,11 +69,10 @@ kubectl wait --for=condition=Ready --timeout=-1s  pods --all -n argocd
 kubectl apply -n argocd -f /vagrant/config/argocd_ingress.yaml
 kubectl apply -n argocd -f /vagrant/config/argocd_application_deploy.yaml
 
-
-
-
 echo "PASSWORD:"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
-while true; do kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8090:443; sleep 2; done
+while true; do kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8090:443; sleep 2; done &
+while true; do kubectl port-forward --address 0.0.0.0 svc/will-playground-service -n dev 8888:8888; sleep 2; done &
+
 
