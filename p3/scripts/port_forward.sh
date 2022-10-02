e3030d425fd5c3dd0cfac7145788f06b542f67ec
Tenagrim <tenagrim@gmail.com>
2022-10-02 14:35:37 +0500

    p3 port forward debub message

--- p3/scripts/port_forward.sh
+++ p3/scripts/port_forward.sh
@@ -2,5 +2,7 @@
 
 export PATH=$PATH:/usr/local/bin
 
+echo "[DEBUG] port forwarding launched: svc/$1 $2:$3"
+
 #while true; do kubectl port-forward --address 0.0.0.0 svc/$1 -n $4 $2:$3 >/dev/null 2>/dev/null; sleep 10; done
 while true; do kubectl port-forward --address 0.0.0.0 svc/$1 -n $4 $2:$3 ; sleep 10; done
